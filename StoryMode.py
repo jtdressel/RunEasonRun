c51e79e43ee84a6de7b31d5816137009688553c1
Brandon Shea <shea.goog.25@gmail.com>
2013-04-17 19:09:16 -0400

    update
    update

--- StoryMode.py
+++ StoryMode.py
@@ -12,23 +12,12 @@ class StoryMode(GameMode):
     def __init__(self):
         self.ptr = 0
         self.lst_mode = [self, SpeedMode('city.png', False), BrawlMode('street.png', 224, 385, False), \
-<<<<<<< HEAD
                          SpeedMode('industrial.png', False), BrawlMode('warehouse.png', 290, 380, False)]
         self.mode_name = ['menu_mode', 'city_run', 'street_fight', 'industrial_run', 'warehouse_fight']
         self.modes = ModeManager()
         for i in range(5):
             self.modes.register_mode(self.mode_name[i], self.lst_mode[i])
-=======
-                        SpeedMode('industrial.png', False), BrawlMode('warehouseAlley1.png', 224, 385, False)]
-        self.mode_name = ['menu_mode', 'city_run', 'street_fight', \
-                        'industrial_run', 'warehouse_fight']
-        self.modes = ModeManager()
-        for i in range(5):
-            self.modes.register_mode(self.mode_name[i], self.lst_mode[i])
-        # self.modes.register_mode(self.mode_name[0], self.lst_mode[0])
-        # self.modes.register_mode(self.mode_name[1], self.lst_mode[1])
-        # self.modes.register_mode(self.mode_name[2], self.lst_mode[2])
->>>>>>> dfcd61b6ce38cb7fea7e5ab3d78f2aa7de7701b4
+
         self.level = 1
     
     def enter(self):
